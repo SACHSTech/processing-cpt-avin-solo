ed8669d0ef5b052c16a5c1f337e2f73a78cfbb27
koolcid Ahmadi <99352795+koolcid13@users.noreply.github.com>
2022-06-15 16:14:24 +0000

    stick movement applied

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,51 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	PImage imgBoard;
+  PImage imgStick;
+  PImage imgWhite;
+
+  int[][] ballPos = new int[16][2];
+  double stickX = 250, stickY = 100;
+  double rotation = 0;
+  boolean tempShit = false;
+
   public void settings() {
-	// put your size call here
-    size(400, 400);
+    size(750, 422);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
-    background(210, 255, 173);
+
+    imgBoard = loadImage ("board.png");
+    imgStick = loadImage ("stick.png");
+    imgWhite = loadImage ("white.png");
+
+    ballPos[0][0] = width / 2;
+    ballPos[0][1] = height / 2;
+
+    stickX = ballPos[0][0] + (-0.5 * imgStick.width);
+    stickY = ballPos[0][1] + (-0.5 * imgStick.height);
+   
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+	    image (imgBoard, 0, 0);
+      //image (imgWhite, ballPos[0][0], ballPos[0][1]);
+      //image (imgStick, stickX, stickY);
+      ellipse (ballPos[0][0], ballPos[0][1], 24, 24);
+      pushMatrix();
+      translate ((float) (stickX + imgStick.width / 2), (float) (stickY + imgStick.height / 2));
+      rotate ((float) rotation);
+      translate ((float) (-1.05 * imgStick.width), (float) (-0.5 * imgStick.height));
+      image (imgStick, 0, 0);
+      popMatrix();
+      //rotation = rotation + 0.05;
   }
-  
-  // define other methods down here.
+
+  public void mouseDragged () {
+    rotation = atan2((float) (mouseY - stickY), (float) (mouseX - stickX));
+  }
+ 
 }
