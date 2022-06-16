571a64a31205934d8f75ba19a40aa0181b510dfa
koolcid Ahmadi <99352795+koolcid13@users.noreply.github.com>
2022-06-16 22:34:39 +0000

    -edge collision detection added -friction happens -balls sometimes overlap still

--- Sketch.java
+++ Sketch.java
@@ -13,9 +13,9 @@ public class Sketch extends PApplet {
   double stickX = 250, stickY = 100;
   double rotation = 0;
   int defaultPower = 200;
-  boolean hideStick = false, isHit = false;
   double xChange = 0, yChange = 0;
   double slope = 1;
+  int ballsMoving = 0;
 
   public void settings() {
     size(750, 422);
@@ -30,43 +30,38 @@ public class Sketch extends PApplet {
     ballPos[0][1] = 211;
     ballPos[1][0] = 537;
     ballPos[1][1] = 211;
-    ballPos[2][0] = 555;
-    ballPos[2][1] = 201;
-    ballPos[3][0] = 555;
-    ballPos[3][1] = 221;
-    ballPos[4][0] = 573;
-    ballPos[4][1] = 191;
-    ballPos[5][0] = 573;
+    ballPos[2][0] = 556;
+    ballPos[2][1] = 200;
+    ballPos[3][0] = 556;
+    ballPos[3][1] = 222;
+    ballPos[4][0] = 575;
+    ballPos[4][1] = 189;
+    ballPos[5][0] = 575;
     ballPos[5][1] = 211;
-    ballPos[6][0] = 573;
-    ballPos[6][1] = 231;
-    ballPos[7][0] = 591;
-    ballPos[7][1] = 181;
-    ballPos[8][0] = 591;
-    ballPos[8][1] = 201;
-    ballPos[9][0] = 591;
-    ballPos[9][1] = 221;
-    ballPos[10][0] = 591;
-    ballPos[10][1] = 241;
-    ballPos[11][0] = 609;
-    ballPos[11][1] = 171;
-    ballPos[12][0] = 609;
-    ballPos[12][1] = 191;
-    ballPos[13][0] = 609;
+    ballPos[6][0] = 575;
+    ballPos[6][1] = 233;
+    ballPos[7][0] = 594;
+    ballPos[7][1] = 178;
+    ballPos[8][0] = 594;
+    ballPos[8][1] = 200;
+    ballPos[9][0] = 594;
+    ballPos[9][1] = 222;
+    ballPos[10][0] = 594;
+    ballPos[10][1] = 244;
+    ballPos[11][0] = 613;
+    ballPos[11][1] = 167;
+    ballPos[12][0] = 613;
+    ballPos[12][1] = 189;
+    ballPos[13][0] = 613;
     ballPos[13][1] = 211;
-    ballPos[14][0] = 609;
-    ballPos[14][1] = 231;
-    ballPos[15][0] = 609;
-    ballPos[15][1] = 251;
+    ballPos[14][0] = 613;
+    ballPos[14][1] = 233;
+    ballPos[15][0] = 613;
+    ballPos[15][1] = 255;
 
 
     
     //upd stickX and stickY after hits
-
-    stickX = ballPos[0][0] + (-0.5 * imgStick.width);
-    stickY = ballPos[0][1] + (-0.5 * imgStick.height);
-    velocity[0][0] = 1000;
-    velTemp = velocity[0][0];
 
     for (int i = 1; i < 16; i ++) {
       velocity[i][0] = 0;
@@ -76,6 +71,18 @@ public class Sketch extends PApplet {
   }
 
   public void draw() {
+    
+    stickX = ballPos[0][0] + (-0.5 * imgStick.width);
+    stickY = ballPos[0][1] + (-0.5 * imgStick.height);
+    ballsMoving = 0;
+    for (int i = 0; i < 16; i ++) {
+      for (int j = 0; j < 2; j ++) {
+        if (velocity[i][j] != 0) {
+          ballsMoving ++;
+          break;
+        }
+      }
+    }
 	  image (imgBoard, 0, 0);
     fill (255);
     ellipse ((float) ballPos[0][0], (float) ballPos[0][1], 20, 20); //add conditions of when moving
@@ -94,53 +101,56 @@ public class Sketch extends PApplet {
       }
     }
       
+    System.out.println (ballsMoving);
     
     pushMatrix();
     translate ((float) (stickX + imgStick.width / 2), (float) (stickY + imgStick.height / 2));
     rotate ((float) (rotation));
     translate ((float) (-1.05 * imgStick.width), (float) (-0.5 * imgStick.height));
-    if (hideStick == false) {
+    if (ballsMoving == 0) {
       image (imgStick, 0, 0);
       // if slope is undefined?
       slope = -yChange / xChange;
-      velocity[0][1] = (float) slope * (-velocity[0][0]);
     }
     popMatrix();
-    
-    if (isHit == true) {
-      if (velTemp > 0) { //ballSpeed?
-        velTemp --;
-         /*if (ballPos[0][0] + 10 > width - 50 || ballPos[0][0] - 10 < 50) {
-          ballSpeed[0][0] *= -1;
-        }
-        if (ballPos[0][1] + 10 > height - 50 || ballPos[0][1] - 10 < 50) {
-          ballSpeed[0][1] *= -1;
-        }
-        */
-        for (int i = 0; i < 16; i ++) {
-          ballSpeed[i][0] = (float) (velocity[i][0] / 100); //add acceleration?
-          ballSpeed[i][1] = (float) (velocity[i][1] / 100);
-          ballPos[i][0] += ballSpeed[i][0];
-          ballPos[i][1] += ballSpeed[i][1];
+    for (int i = 0; i < 16; i ++) {
+      velocity[i][0] *= 0.97;
+      velocity[i][1] *= 0.97;
+      for (int j = 0; j < 2; j ++) {
+        if (velocity[i][j] < 2) {
+          velocity[i][j] = 0;
         }
       }
-      
+      ballSpeed[i][0] = (float) (velocity[i][0] / 100); //add acceleration?
+      ballSpeed[i][1] = (float) (velocity[i][1] / 100);
+      ballPos[i][0] += ballSpeed[i][0];
+      ballPos[i][1] += ballSpeed[i][1];
+
+      if (ballPos[i][0] + 10 > width - 50 || ballPos[i][0] - 10 < 50) {
+        velocity[i][0] *= -1;
+      }
+      if (ballPos[i][1] + 10 > height - 50 || ballPos[i][1] - 10 < 50) {
+        velocity[i][1] *= -1;
+      }
     }
   }
 
-  public void mouseDragged () { //not simply dragged, level stages needed.
+  public void mouseMoved () { //not simply dragged, level stages needed.
     
     rotation = atan2((float) (mouseY - ballPos[0][1]), (float) (mouseX - ballPos[0][0]));
     xChange = 1.05 * imgStick.width * Math.cos (rotation);
     yChange = 1.05 * imgStick.width * Math.sin (rotation);
     slope = -yChange / xChange;
+  }
+
+  public void mouseClicked () { //set a stage cause clicking during ruins directions
     if (Math.cos (rotation) < 0) {
-      velocity[0][0] = -200;
+      velocity[0][0] = -1000;
     }
     else {
-      velocity[0][0] = 200;
+      velocity[0][0] = 1000;
     }
-    if (Math.sin (rotation) < 0) {
+    if (Math.sin (rotation) > 0) {
       velocity[0][1] = Math.abs (velocity[0][0] * (float) slope);
     }
     else {
@@ -148,21 +158,15 @@ public class Sketch extends PApplet {
     }
   }
 
-  public void mouseClicked () { //set a stage cause clicking during ruins directions
-    isHit = true;
-    hideStick = true;
-    
-  }
-
   private boolean collisionCheck (int a, int b) {
-    if (dist ((float) ballPos[a][0], (float) ballPos[a][1], (float) ballPos[b][0], (float) ballPos[b][1]) < 20) {
+    if (dist ((float) ballPos[a][0], (float) ballPos[a][1], (float) ballPos[b][0], (float) ballPos[b][1]) < 21) {
       return true;
     }
     return false;
   }
 
   private void collided (int a, int b) {
-    System.out.println (a +  " " + b);
+    //System.out.println (a + " " + b);
     float normalX = (float) (ballPos[b][0] - ballPos[a][0]);
     float normalY = (float) (ballPos[b][1] - ballPos[a][1]);
 
@@ -190,8 +194,6 @@ public class Sketch extends PApplet {
     velocity[a][1] = aVelDotNormTagY + aVelDotTangentTagY;
     velocity[b][0] = bVelDotNormTagX + bVelDotTangentTagX;
     velocity[b][1] = bVelDotNormTagY + bVelDotTangentTagY;
-
-
 
   }
  
